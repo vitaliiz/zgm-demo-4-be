0b678f2f18207c9ddb414c31a94704eb205c7325
vitaliiz <vzhebrakovskyi@gmail.com>
2026-05-14 16:46:08 +0200

    WIP

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,26 +1,38 @@
 package com.example
 
-import io.ktor.http.ContentType
+import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.http.content.staticFiles
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
-import java.io.File
 import org.example.*
 import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun configureRouting(app: Application) {
     DatabaseFactory.init()
     app.routing {
-        staticFiles("/media", File("data")) {
-            contentType { _ -> ContentType.Audio.MPEG }
-        }
         get("/dialogs") {
             val dialogsList = buildDialogList()
             val response = ExampleData(dialogsList)
             call.respondText(Json.encodeToString(response))
+        }
+        get("/media/voice/{lang}/{hash}") {
+            val lang = call.parameters["lang"] ?: return@get call.respond(HttpStatusCode.BadRequest)
+            val hash = call.parameters["hash"] ?: return@get call.respond(HttpStatusCode.BadRequest)
+
+            val voiceData = transaction {
+                VoiceTable.selectAll()
+                    .where { (VoiceTable.lang eq lang) and (VoiceTable.hashEn eq hash) }
+                    .singleOrNull()?.get(VoiceTable.data)
+            }
+
+            if (voiceData != null) {
+                call.respondBytes(voiceData.bytes, ContentType.Audio.MPEG)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
         }
     }
 }
