d2ff2460bf63d2cfc7708249d6101f2a957a9aad
vitaliiz <vzhebrakovskyi@gmail.com>
2026-06-30 21:03:10 +0200

    WIP

--- src/main/kotlin/DialogBuilder.kt
+++ src/main/kotlin/DialogBuilder.kt
@@ -37,7 +37,7 @@ fun buildDialogList(): List<ExampleDialog> = transaction {
                     text = ExampleTextWithAudio(
                         native = wordText,
                         foreign = getTranslation("NL", wordText),
-                        audioUrl = "http://192.168.2.74:8080/media/voice/NL/${toHash(wordText)}"
+                        audioUrl = "http://167.99.215.169:8080/media/voice/NL/${toHash(wordText)}"
                     )
                 )
             }
@@ -82,7 +82,7 @@ fun buildDialogList(): List<ExampleDialog> = transaction {
                             variableId = s.variableId,
                             native = s.sentenceText,
                             foreign = getTranslation("NL", s.sentenceText),
-                            audioUrl = "http://192.168.2.74:8080/media/voice/NL/${toHash(s.sentenceText)}"
+                            audioUrl = "http://167.99.215.169:8080/media/voice/NL/${toHash(s.sentenceText)}"
                         )
                     }
                 )
