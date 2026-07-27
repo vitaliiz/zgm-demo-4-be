9fc862d5a36bae4640351ff46cd9c30061d6312b
vitaliiz <vzhebrakovskyi@gmail.com>
2026-07-27 19:41:27 +0200

    WIP

--- src/main/kotlin/DbConnection.kt
+++ src/main/kotlin/DbConnection.kt
@@ -7,14 +7,9 @@ import org.jetbrains.exposed.sql.transactions.transaction
 object DatabaseFactory {
     fun init() {
         val driverClassName = "org.postgresql.Driver"
-        val jdbcUrl = "jdbc:postgresql://167.99.211.213:5432/zgm"
+        val jdbcUrl = "jdbc:postgresql://localhost:5432/zgm"
         val user = "postgres"
         val password = "pass"
         Database.connect(jdbcUrl, driverClassName, user, password)
-        transaction {
-            SchemaUtils.createMissingTablesAndColumns(
-                WordTable, RoleTable, SentenceTable, DialogTable, DialogFlowStepSentenceTable, DialogFlowSelectionTable, TranslationTable, VoiceTable
-            )
-        }
     }
 }
